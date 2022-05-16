4d156f26e5219318ce67c188d429426a877bd2e0
ts1g18 <ts1g18@soton.ac.uk>
2022-05-16 20:42:31 +0100

    Scale data, implement LDA model and evaluate its accuracy

--- ModellingData.py
+++ ModellingData.py
@@ -4,7 +4,9 @@
 
 import pandas
 import numpy as np
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, RepeatedStratifiedKFold, cross_val_score
+from sklearn import preprocessing
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
 
 # this method reads the training data
@@ -38,3 +40,21 @@ def read_testing_data():
 x_train, x_test, y_train, y_test = train_test_split(read_training_data()[0], read_training_data()[1], test_size=0.2,
                                                     random_state=0)
 
+# scale data to make it easy for model to learn and understand the problem
+# simple way is to use sklearn preprocessing to use MinMaxScaler()
+scaler = preprocessing.MinMaxScaler()
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.fit_transform(x_test)
+x_pred = scaler.fit_transform(read_testing_data())
+x_all = scaler.fit_transform(read_training_data()[0])
+
+# Fit the LDA model
+model = LinearDiscriminantAnalysis()
+model.fit(x_train, y_train)
+y_pred = model.predict(x_pred)
+y_pred = [int(x) for x in y_pred]
+# Define method to evaluate model
+cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+# evaluate model
+scores = cross_val_score(model, x_all, read_training_data()[1], scoring='accuracy', cv=cv, n_jobs=-1)
+print('Mean Accuracy: %.3f (%.3f)' % (np.mean(scores), np.std(scores)))
