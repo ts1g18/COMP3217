cd5edbd56c8830559ed1eb70a4fc9df0ea597347
ts1g18 <ts1g18@soton.ac.uk>
2022-05-16 14:35:01 +0100

    - Created ModellingData.py

---
+++ ModellingData.py
@@ -0,0 +1,4 @@
+# This class implements a model to train the testing data based on the training data provided
+# Its purpose is to provide the predicted binary values (0 or 1) based on the training algorithm implemented
+# The predictions for the testing data are provided in the TestingResults.txt as requested on the specification
+
