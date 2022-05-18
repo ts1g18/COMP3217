3241fb1b4ae1d2fbc94466d565a63c370abe052e
ts1g18 <ts1g18@soton.ac.uk>
2022-05-18 13:00:16 +0100

    Storing all tasks

--- schedule.py
+++ schedule.py
@@ -8,12 +8,23 @@ def read_tasks():
     # COMP3217CW2Input.xlsx contains the user task information
     task_file = pandas.read_excel('COMP3217CW2Input.xlsx', sheet_name='User & Task ID')
     # print(task_file)
-    user_and_task_id = task_file['User & Task ID'].tolist()
+    user_tasks = task_file['User & Task ID'].tolist()
     ready_time = task_file['Ready Time'].tolist()
     deadline = task_file['Deadline'].tolist()
     max_energy_per_hour = task_file['Maximum scheduled energy per hour'].tolist()
     energy_demand = task_file['Energy Demand'].tolist()
-    return user_and_task_id, ready_time, deadline, max_energy_per_hour, energy_demand
+
+    # this array will contain all tasks and each task is an array itself
+    all_tasks = []
+
+    for i in range(len(user_tasks)):
+        task = [ready_time[i], deadline[i], max_energy_per_hour[i], energy_demand[i]]
+
+        all_tasks.append(task)
+
+    print(user_tasks)
+
+    return all_tasks, user_tasks, ready_time, deadline, max_energy_per_hour, energy_demand
 
 
 read_tasks()
